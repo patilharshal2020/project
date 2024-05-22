a39dc7185f80c413ce704351ac7198d57868c700
harshal <patilharshal675@gmail.com>
2024-05-22 17:10:31 +0530

    fromEvent observable

--- harry/src/user/user-list/user-list.component.ts
+++ harry/src/user/user-list/user-list.component.ts
@@ -1,46 +1,36 @@
-import { Component, DoCheck, Input, OnChanges, OnInit } from '@angular/core';
-import { Observable, from, of } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { from, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent implements OnInit, OnChanges, DoCheck {
+export class UserListComponent implements OnInit, AfterViewInit {
   @Input() title = '';
   data: any[] = [];
-  arr1: number[] = [1,2,3,4,5];
-  arr2: string[] = ['A','B','C','D','E'];
-  // myObs = new Observable((observer) => {
-  //   setTimeout(() => {observer.next(1)}, 1000);
-  //   setTimeout(() => {observer.next(2)}, 2000);
-  //   setTimeout(() => {observer.next(3)}, 8000);
-  //   setTimeout(() => {observer.complete()}, 3000);
-  //   setTimeout(() => {observer.next(4)}, 4000);
-  //   setTimeout(() => {observer.next(5)}, 5000);
-  // })
+  @ViewChild('createBtn')
+  createItem: ElementRef;
+  createItmObs;
 
-  myPromise = new Promise((resolve)=>{
-    resolve([1,2,3,4,5]);
-  })
-
-  myObs = from(this.myPromise);
-
-  constructor(){
-    console.log('Constructor is here');
-  }
-  ngOnChanges(){
-    console.log('onChanges hook is here');
-  }
   ngOnInit(): void {
-    console.log('onInit hook is here');
-    this.myObs.subscribe(el => {
-      this.data.push(el);
-    },()=>{
-    }, ()=>{
+
+  }
+
+  btnClicked(){
+    let count = 1;
+    this.createItmObs = fromEvent(this.createItem.nativeElement, 'click').subscribe(item =>{
+      this.addItem(count++)
     })
   }
-  ngDoCheck(){
-    console.log('ngDoCheck hook is here');
+
+  ngAfterViewInit(){
+    this.btnClicked();
+  }
+
+  addItem(count){
+    const item = document.createElement('li');
+    item.innerHTML = 'list item'+ count;
+    document.getElementById('container').appendChild(item)
   }
 }
